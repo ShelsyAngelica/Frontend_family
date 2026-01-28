ec742e5e656db7f47add329577fb54623dfc9806
Shelsy Angelica <shelsyangelik@gmail.com>
2026-01-28 00:04:51 -0500

    correccion  para detectar si estamos en local o en produccion

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,10 @@
 // Detecta automáticamente si estamos en local o producción
 const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
 
-export const API_URL = isLocal 
-    ? 'http://localhost:8000/api' 
-    : 'shelsygarcia.alwaysdata.net/api';
+let apiUrl = '';
+if (isLocal) {
+    apiUrl = 'http://localhost:8000/api';
+} else {
+    apiUrl = 'shelsygarcia.alwaysdata.net/api';
+}
+export const API_URL = apiUrl;
